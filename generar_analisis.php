7c8f820f8039f222aa40b0a9c3822844d81bdce9
noelia0401 <noelia.alvvic@educa.jcyl.es>
2021-11-17 14:23:03 +0100

    a

--- generar_analisis.php
+++ generar_analisis.php
@@ -1,6 +1,33 @@
 <?php
-
+session_start();
 include 'head.php';
+
+
+$r1=0;
+$r2=0;
+$r3=0;
+$r4=0;
+
+foreach ($_SESSION['multas'] as $clave)
+ {
+   foreach ($clave as $valor=>$dato)
+   {
+      if ($valor=="radar"){
+        switch ($dato){
+          case 1: $r1++;
+          break;
+          case 2: $r2++;
+          break;
+          case 3: $r3++;
+          break;
+          case 4: $r4++;
+          break;
+        }
+      }
+   }
+ }
+
+
 echo'Analisis de las Multas por Radares <mark>(1.5 Puntos)<br><br>
 <table>
   <thead>
@@ -13,10 +40,10 @@ echo'Analisis de las Multas por Radares <mark>(1.5 Puntos)<br><br>
   </thead>
   <tbody>
     <tr>
-      <td>10</td>
-      <td>20</td>
-      <td>15</td>
-      <td>67</td>
+      <td>'.$r1.'</td>
+      <td>'.$r2.'</td>
+      <td>'.$r3.'</td>
+      <td>'.$r4.'</td>
     </tr>
     
   </tfoot>
